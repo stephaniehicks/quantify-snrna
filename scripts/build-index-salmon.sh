a352e506468c4bdf645805e039cfec2041733c7a
Stephanie Hicks <stephaniechicks@gmail.com>
2019-12-06 12:57:02 -0500

    start to modify code for here package

--- scripts/build-index-salmon.sh
+++ scripts/build-index-salmon.sh
@@ -2,7 +2,5 @@
 #$ -cwd
 #$ -l mem_free=40G,h_vmem=45G
 
-cd /users/shicks1/data/reference/genomes/hsapiens/GENCODE/release_32
-
 # create salmon index (3-5 mins)
-salmon index --gencode -t gencode.v32.preandmrna.fa.gz -i /fastscratch/myscratch/shicks1/alsf-filbin/gencode.v32_salmon-index-v1.0.0 --keepDuplicates --threads 4
+salmon index -t /users/shicks1/data/reference/genomes/hsapiens/GENCODE/release_32/gencode.v32.preandmrna.fa.gz -i /fastscratch/myscratch/shicks1/alsf-filbin/gencode.v32_salmon-index-v1.0.0 --keepDuplicates --threads 4
