2d50b28c6a9395e8a15fa0f76fd43d327a2bf5b0
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2019-12-09 23:33:24 -0500

    Create path variable and reserve nodes

--- scripts/run-salmon.sh
+++ scripts/run-salmon.sh
@@ -1,17 +1,19 @@
 #$ -pe local 6
+#$ -R y
 #$ -cwd
 #$ -l mem_free=10G,h_vmem=15G
 
-# for fn in `cat /fastscratch/myscratch/shicks1/alsf-filbin/sample_data/unique_cell_paths.txt`;
-for fn in `cat /fastscratch/myscratch/akuo/alsf-filbin/sample_data/unique_cell_paths.txt`; 
+# set project directory path
+# d=/fastscratch/myscratch/shicks1/alsf-filbin
+d=/fastscratch/myscratch/akuo/alsf-filbin
+
+for fn in `cat $d/sample_data/unique_cell_paths.txt`; 
 do 
 samp=`basename ${fn}`
 samp=${samp::-2}
 echo "Processing sample ${samp}"
-# salmon quant -i /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0 -l A \
-salmon quant -i /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0 -l A \
+salmon quant -i $d/salmon_files/gencode.v32_salmon-index-v1.0.0 -l A \
          -1 ${fn}1.fastq.gz \
          -2 ${fn}2.fastq.gz \
-#         -p 6 --validateMappings -o /fastscratch/myscratch/shicks1/alsf-filbin/salmon_quants/${samp}_quant
-         -p 6 --validateMappings -o /fastscratch/myscratch/akuo/alsf-filbin/salmon_quants/${samp}_quant
+         -p 6 --validateMappings -o $d/salmon_quants/${samp}_quant
 done
