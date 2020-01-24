0b1ff64ef22c424d0997a232f3e031ba79fcc192
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-01-24 16:05:26 -0500

    Change to using mRNA reference transcripts

--- scripts/build-index-salmon.sh
+++ scripts/build-index-salmon.sh
@@ -3,5 +3,5 @@
 #$ -l mem_free=15G,h_vmem=20G
 
 # create salmon index (this process takes ~2-3 hours)
-# salmon index -t /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gencode.v32.preandmrna.fa.gz -i /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0 --gencode --threads 4
-salmon index -t /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/gencode.v32.preandmrna.fa.gz -i /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0 --gencode --threads 4
+salmon index -t /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gencode.v32.transcripts.fa.gz -i /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0 --gencode --threads 4
+# salmon index -t /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/gencode.v32.transcripts.fa.gz -i /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0 --gencode --threads 4
