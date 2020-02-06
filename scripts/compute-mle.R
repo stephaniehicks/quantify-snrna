b830e8bb20460ea97fc0424288ae434f50c6eb01
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-02-06 12:31:57 -0500

    Fix errors to use dataset parameter

--- scripts/compute-mle.R
+++ scripts/compute-mle.R
@@ -6,17 +6,17 @@
 # Run function to compute poisson lognormal mle parameters
 
 library(here)
+library(SingleCellExperiment)
 source(here("scripts", "quminorm.R"))
 
 # Load filtered counts
-filtered_file_name = here("salmon_quants", "dat_filtered.rds")
-if(file.exists(filtered_file_name)){
-  dat_filtered = readRDS(filtered_file_name)
-}
+dataset = "mrna"
+filtered_file_name = here("salmon_quants", paste0("sce_filtered_", dataset, ".rds"))
+sce_filtered = readRDS(filtered_file_name)
+dat_filtered = counts(sce_filtered)
 
 # Find mle parameters mu and sig
-use_matrix = T
-dataset = "mrna"
+use_matrix = F
 if(use_matrix){
   m = round(dat_filtered)
   res = poilog_mle_matrix(m)
@@ -28,5 +28,5 @@ if(use_matrix){
   x = dat_filtered[, i]
   x = round(x) # counts must be integers
   res = poilog_mle(x)
-  saveRDS(res, here(paste0("mle_results_" dataset), paste0(i, ".rds")))
+  saveRDS(res, here(paste0("mle_results_", dataset), paste0(i, ".rds")))
 }
