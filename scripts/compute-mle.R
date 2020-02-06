0dcc7e4790fa16c2e154f67055b350b75c70a727
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-02-06 11:54:41 -0500

    Add dataset parameter

--- scripts/compute-mle.R
+++ scripts/compute-mle.R
@@ -1,7 +1,7 @@
 # compute-mle.R
 # -----------------------------------------------------------------------------
 # Author:             Albert Kuo
-# Date last modified: Jan 8, 2020
+# Date last modified: Feb 6, 2020
 #
 # Run function to compute poisson lognormal mle parameters
 
@@ -16,16 +16,17 @@ if(file.exists(filtered_file_name)){
 
 # Find mle parameters mu and sig
 use_matrix = T
+dataset = "mrna"
 if(use_matrix){
   m = round(dat_filtered)
   res = poilog_mle_matrix(m)
-  saveRDS(res, here("mle_results", "all.rds"))
+  saveRDS(res, here(paste0("mle_results_", dataset), "all.rds"))
 } else {
-  #$ -t 1-534
+  #$ -t 1-576
   #$ -tc 50
   i = as.integer(Sys.getenv("SGE_TASK_ID")) 
   x = dat_filtered[, i]
   x = round(x) # counts must be integers
   res = poilog_mle(x)
-  saveRDS(res, here("mle_results", paste0(i, ".rds")))
+  saveRDS(res, here(paste0("mle_results_" dataset), paste0(i, ".rds")))
 }
