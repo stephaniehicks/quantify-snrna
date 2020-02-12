83ebd909d741a8c7be1c45887066ebc98d0bba2c
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-02-12 17:02:39 -0500

    Change file path

--- scripts/build-index-salmon.sh
+++ scripts/build-index-salmon.sh
@@ -5,4 +5,4 @@
 # create salmon index (this process takes ~2-3 hours)
 # make sure you are using the right index (for mRNA, pre-mRNA, or both)
 # salmon index -t /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/GRCh38.premRNA.fa.gz -i /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0-premRNA --gencode --threads 4
-salmon index -t /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/gencode.v32.transcripts.fa.gz -i /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0-mRNA --gencode --threads 4
+salmon index -t /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gencode.v32.transcripts.fa.gz -i /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0-mRNA --gencode --threads 4
