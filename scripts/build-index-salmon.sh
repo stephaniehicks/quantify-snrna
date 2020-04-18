21834f00c7d71405005c250faf799aee90f51a96
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-04-18 15:17:29 -0400

    Increase file size limit

--- scripts/build-index-salmon.sh
+++ scripts/build-index-salmon.sh
@@ -2,11 +2,11 @@
 #$ -cwd
 #$ -o log/
 #$ -e log/
-#$ -l mem_free=30G,h_vmem=30G
+#$ -l mem_free=30G,h_vmem=30G,h_fsize=30G
 
 # create salmon index (this process takes ~2-3 hours)
 # make sure you are using the right transcriptome (for mRNA, pre-mRNA, intron, or some combination)
 # salmon index -t /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/GRCh38.premRNA.fa.gz -i /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0-premRNA --gencode --threads 4
-salmon index -t /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gentrome_transcripts.fa.gz \
+salmon index -t /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gentrome_preandmrna.fa.gz \
                 -d /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/decoys.txt \
-                -i /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0-transcripts --gencode --threads 4
+                -i /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0-preandmrna --gencode --threads 4
