dcb72c041385a366e49e401af97c9d8bbd6b53c2
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-05-28 16:33:23 -0400

    Remove fasterq and core request

--- scripts/download-geo-data.sh
+++ scripts/download-geo-data.sh
@@ -1,4 +1,3 @@
-#$ -pe local 4
 #$ -cwd
 #$ -o log/
 #$ -e log/
@@ -7,4 +6,3 @@
 d=/fastscratch/myscratch/akuo/alsf-filbin/files/
 
 prefetch --option-file $d/SRR_files.txt
-cat $d/SRR_files.txt | parallel -j 4 fasterq-dump {}.sra
