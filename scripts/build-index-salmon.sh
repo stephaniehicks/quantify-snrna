deac6f6c83003036e4ebd69658aaed544c9b208c
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-06-09 17:31:04 -0400

    Use mouse files

--- scripts/build-index-salmon.sh
+++ scripts/build-index-salmon.sh
@@ -7,6 +7,6 @@
 # create salmon index (this process takes ~2-3 hours)
 # make sure you are using the right transcriptome (for mRNA, pre-mRNA, intron, or some combination)
 # salmon index -t /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/GRCh38.premRNA.fa.gz -i /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0-premRNA --gencode --threads 4
-salmon index -t /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gentrome_preandmrna.fa.gz \
-                -d /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/decoys.txt \
-                -i /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0-preandmrna --gencode --threads 4
+salmon index -t /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gentrome_transcripts_mouse.fa.gz \
+                -d /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/decoys_mouse.txt \
+                -i /fastscratch/myscratch/akuo/alsf-filbin/salmon_files/gencode.v32_salmon-index-v1.0.0-transcripts-mouse --gencode --threads 4
--- scripts/run-alevin.sh
+++ scripts/run-alevin.sh
@@ -7,7 +7,7 @@
 # d=/fastscratch/myscratch/shicks1/alsf-filbin
 d=/fastscratch/myscratch/akuo/alsf-filbin
 
-salmon alevin -l ISR -1 SRR9169239_3.fastq -2 SRR9169239_1.fastq --chromium  -i $d/salmon_files/gencode.v32_salmon-index-v1.0.0-transcripts -p 10 -o alevin_output --tgMap txp2gene.tsv
+salmon alevin -l ISR -1 SRR9169239_3.fastq -2 SRR9169239_1.fastq --chromium  -i $d/salmon_files/gencode.v32_salmon-index-v1.0.0-transcripts-mouse -p 10 -o alevin_output --tgMap $d/salmon_files/gencode.v32.annotation.tx2gene.mouse.txt
 
 
 # for fn in `cat $d/sample_data/unique_cell_paths.txt`; 
