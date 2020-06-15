4d39b388eb452d3f4f60798b8f6355362cbacd6d
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-06-15 12:40:13 -0400

    Fix file paths

--- scripts/run-alevin.sh
+++ scripts/run-alevin.sh
@@ -1,13 +1,13 @@
 #$ -pe local 6
 #$ -R y
 #$ -cwd
-#$ -l mem_free=10G,h_vmem=15G
+#$ -l mem_free=50G,h_vmem=50G
 
 # set project directory path
 # d=/fastscratch/myscratch/shicks1/alsf-filbin
 d=/fastscratch/myscratch/akuo/alsf-filbin
 
-salmon alevin -l ISR -1 SRR9169239_3.fastq -2 SRR9169239_1.fastq --chromium  -i $d/salmon_files/gencode.v32_salmon-index-v1.0.0-transcripts-mouse -p 10 -o alevin_output --tgMap $d/salmon_files/gencode.v32.annotation.tx2gene.mouse.txt
+salmon alevin -l ISR -1 $d/SRR9169239_3.fastq $d/SRR9169239_4.fastq -2 $d/SRR9169239_1.fastq $d/SRR9169239_2.fastq --chromium  -i $d/salmon_files/gencode.v32_salmon-index-v1.0.0-transcripts-mouse -p 10 -o alevin_output --tgMap $d/salmon_files/gencode.v32.annotation.tx2gene.mouse.txt
 
 
 # for fn in `cat $d/sample_data/unique_cell_paths.txt`; 
