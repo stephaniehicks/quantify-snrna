a7abc23aeb79d5e44bff6610095f28e4134c7f5d
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-06-15 12:39:52 -0400

    Use fasterq

--- scripts/convert-geo-data.sh
+++ scripts/convert-geo-data.sh
@@ -8,5 +8,7 @@ d=/fastscratch/myscratch/akuo/alsf-filbin/files
 for file in $(<$d/SRR_files.txt)
 do
     echo "$file.sra"
-    fastq-dump -O "/fastscratch/myscratch/akuo/alsf-filbin/sample_data/geo/sra/" --split-files "/fastscratch/myscratch/akuo/alsf-filbin/sample_data/geo/sra/$file.sra"
+    # fastq-dump -O "/fastscratch/myscratch/akuo/alsf-filbin/sample_data/geo/sra/" --split-files "/fastscratch/myscratch/akuo/alsf-filbin/sample_data/geo/sra/$file.sra"
+    fasterq-dump -O "/fastscratch/myscratch/akuo/alsf-filbin/sample_data/geo/sra/" --split-files --include-technical "/fastscratch/myscratch/akuo/alsf-filbin/sample_data/geo/sra/$file.sra"
+
 done
