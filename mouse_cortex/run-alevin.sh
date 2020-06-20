e1584719d7b1a8b2f22668cb7f0daef043c155bc
Stephanie Hicks <stephaniechicks@gmail.com>
2020-06-20 19:03:14 -0400

    starting to edit the run-alevin shell script

--- mouse_cortex/run-alevin.sh
+++ mouse_cortex/run-alevin.sh
@@ -1,13 +1,24 @@
-#$ -pe local 6
+#$ -pe local 10
 #$ -R y
 #$ -cwd
-#$ -l mem_free=10G,h_vmem=15G
+#$ -o log/
+#$ -e log/
+#$ -l mem_free=5G,h_vmem=10G
 
 # set project directory path
-# d=/fastscratch/myscratch/shicks1/alsf-filbin
-d=/fastscratch/myscratch/akuo/alsf-filbin
+d=/fastscratch/myscratch/shicks1/alsf-filbin
+f=/fastscratch/myscratch/shicks1/sra
+# d=/fastscratch/myscratch/akuo/alsf-filbin
 
-salmon alevin -l ISR -1 SRR9169239_3.fastq -2 SRR9169239_1.fastq --chromium  -i $d/salmon_files/gencode.v32_salmon-index-v1.0.0-transcripts-mouse -p 10 -o alevin_output --tgMap $d/salmon_files/gencode.v32.annotation.tx2gene.mouse.txt
+salmon alevin -l ISR \ 
+      -1 $f/SRR9169228_1.fastq \ 
+      -2 $f/SRR9169228_2.fastq \ 
+      --chromium  \ 
+      -i $d/salmon_files/mouse/salmon_transcripts_index \ 
+      -p 10 \ 
+      -o $d/mouse_cortex/salmon_quants/ \ 
+      --tgMap $d/salmon_files/mouse/gencode.v32.annotation.tx2gene.mouse.txt 
+      
 
 
 # for fn in `cat $d/sample_data/unique_cell_paths.txt`; 
