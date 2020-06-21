355665bb100b39bacce56a5e48a6fdd94aeb12de
Stephanie Hicks <stephaniechicks@gmail.com>
2020-06-21 15:43:29 -0400

    sch - updating extract-fastq shell script to work for both albert and stephanie

--- mouse_cortex/extract-fastq.sh
+++ mouse_cortex/extract-fastq.sh
@@ -2,12 +2,20 @@
 #$ -cwd
 #$ -o log/
 #$ -e log/
-#$ -l mem_free=40G,h_vmem=40G
+#$ -l mem_free=5G,h_vmem=10G
 
-d=/fastscratch/myscratch/akuo/alsf-filbin/mouse_cortex/00-files/
+a=/fastscratch/myscratch/akuo/alsf-filbin/mouse_cortex/00-files/
+b=/fastscratch/myscratch/shicks1/alsf-filbin
+c=/fastscratch/myscratch/akuo/alsf-filbin
 
-for file in $(<$d/SRR_files_10x.txt)
+
+for file in $(<$a/SRR_files_10x.txt)
 do
     echo "$file.sra"
-     fastq-dump -O "/fastscratch/myscratch/akuo/alsf-filbin/sample_data/geo/sra/" --split-files "/fastscratch/myscratch/akuo/alsf-filbin/sample_data/geo/sra/$file.sra"
+    # stephanie
+    # fastq-dump --split-files --gzip $b/sra/$file
+    
+    # albert
+    fastq-dump -O $c/sample_data/geo/sra/ \
+       --split-files --gzip $c/sample_data/geo/sra/$file
 done
