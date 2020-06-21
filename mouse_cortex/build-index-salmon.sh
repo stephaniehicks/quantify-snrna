ff159fbadc6331946a8ce7db58fd022ce2b0c58a
Stephanie Hicks <stephaniechicks@gmail.com>
2020-06-20 22:55:33 -0400

    sch - trying a smaller kmer size to build salmon index

--- mouse_cortex/build-index-salmon.sh
+++ mouse_cortex/build-index-salmon.sh
@@ -5,9 +5,14 @@
 #$ -l mem_free=5G,h_vmem=10G,h_fsize=30G
 
 # create salmon index (this process takes ~2-3 hours)
-# make sure you are using the right transcriptome (for mRNA, pre-mRNA, intron, or some combination)
-# salmon index -t /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/GRCm38.premRNA.fa.gz \
-#                 -i /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/salmon_transcripts_index_mouse_premRNA --gencode --threads 4
 salmon index -t /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/mouse/gentrome_transcripts.fa.gz \
                 -d /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/mouse/decoys.txt \
-                -i /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/mouse/salmon_transcripts_index --gencode --threads 4
+                -i /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/mouse/salmon_index_gentrome_decoys \
+                --gencode --threads 4
+
+
+# default k-mer size is 31; trying a smaller k-mer size
+salmon index -t /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/mouse/gentrome_transcripts.fa.gz \
+                -d /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/mouse/decoys.txt \
+                -i /fastscratch/myscratch/shicks1/alsf-filbin/salmon_files/mouse/salmon_index_gentrome_decoys_k25 \
+                --gencode --threads 4 -k 25
