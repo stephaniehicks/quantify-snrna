b8e1eef9f92655d521060eaa2e699f818012de30
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-06-22 13:30:23 -0400

    Edit paths gst

--- mouse_cortex/run-alevin.sh
+++ mouse_cortex/run-alevin.sh
@@ -6,19 +6,32 @@
 #$ -l mem_free=5G,h_vmem=10G
 
 # set project directory path
-d=/fastscratch/myscratch/shicks1/alsf-filbin
-f=/fastscratch/myscratch/shicks1/sra
-# d=/fastscratch/myscratch/akuo/alsf-filbin
+#d=/fastscratch/myscratch/shicks1/alsf-filbin
+#f=/fastscratch/myscratch/shicks1/sra
+d=/fastscratch/myscratch/akuo/alsf-filbin
 
+# Albert's
 salmon alevin --libType ISR \
-      --index $d/salmon_files/mouse/salmon_index_gentrome_decoys_k25 \
-      -1 $f/SRR9169228_1.fastq \
-      -2 $f/SRR9169228_2.fastq \
-      --tgMap $d/salmon_files/mouse/gencode.v32.annotation.tx2gene.mouse.txt \
+      --index $d/salmon_files/gencode.v32_salmon-index-v1.0.0-transcripts-mouse \
+      -1 $d/SRR9169236.sra_1.fastq \
+      -2 $d/SRR9169236.sra_2.fastq \
+      --tgMap $d/salmon_files/gencode.v32.annotation.tx2gene.mouse.txt \
       --chromium \
       --threads 10 \
-      --output $d/mouse_cortex/salmon_quants/ \
+      --output $d/ \
       --dumpFeatures --dumpBfh
+
+
+# Stephanie's
+# salmon alevin --libType ISR \
+#       --index $d/salmon_files/mouse/salmon_index_gentrome_decoys_k25 \
+#       -1 $f/SRR9169228_1.fastq \
+#       -2 $f/SRR9169228_2.fastq \
+#       --tgMap $d/salmon_files/mouse/gencode.v32.annotation.tx2gene.mouse.txt \
+#       --chromium \
+#       --threads 10 \
+#       --output $d/mouse_cortex/salmon_quants/ \
+#       --dumpFeatures --dumpBfh
 
 # for fn in `cat $d/sample_data/unique_cell_paths.txt`; 
 # do 
