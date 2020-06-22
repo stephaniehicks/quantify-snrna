ae9dcbe87f2c9dfba53fd839f77e8d3746efeaf9
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-06-22 13:03:16 -0400

    Change order of files

--- scripts/run-alevin.sh
+++ scripts/run-alevin.sh
@@ -1,13 +1,13 @@
-#$ -pe local 6
+#$ -pe local 2
 #$ -R y
 #$ -cwd
-#$ -l mem_free=50G,h_vmem=50G
+#$ -l mem_free=60G,h_vmem=60G
 
 # set project directory path
 # d=/fastscratch/myscratch/shicks1/alsf-filbin
 d=/fastscratch/myscratch/akuo/alsf-filbin
 
-salmon alevin -l ISR -1 $d/SRR9169239_3.fastq $d/SRR9169239_4.fastq -2 $d/SRR9169239_1.fastq $d/SRR9169239_2.fastq --chromium  -i $d/salmon_files/gencode.v32_salmon-index-v1.0.0-transcripts-mouse -p 10 -o alevin_output --tgMap $d/salmon_files/gencode.v32.annotation.tx2gene.mouse.txt
+salmon alevin -l ISR -1 $d/SRR9169236.sra_1.fastq -2 $d/SRR9169236.sra_2.fastq --chromium -i $d/salmon_files/gencode.v32_salmon-index-v1.0.0-transcripts-mouse -p 2 -o $d/alevin_output --tgMap $d/salmon_files/gencode.v32.annotation.tx2gene.mouse.txt
 
 
 # for fn in `cat $d/sample_data/unique_cell_paths.txt`; 
