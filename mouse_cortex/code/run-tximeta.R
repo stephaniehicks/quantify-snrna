e000f5ae8cdb6701ea69e9600eaab0afcceddda4
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-09-04 15:21:50 -0400

    Comment out load linkedTxome

--- mouse_cortex/code/run-tximeta.R
+++ mouse_cortex/code/run-tximeta.R
@@ -5,64 +5,41 @@
 #
 # Use tximeta to read in quant files (output from alevin) into SummarizedExperiment files
 
+library(here)
 suppressPackageStartupMessages({
   library(here)
   library(tximeta)
+  library(fishpond)
+  library(SummarizedExperiment)
 })
 
 pipeline = "transcripts" # "transcripts" or "preandmrna" or "introncollapse/separate"
 
-# Create linkedTranscriptome
-if(pipeline == "transcripts"){
-  index_dir = here("mouse_cortex", "salmon_files", "gencode.vM25_salmon-index-v1.0.0-transcripts-mouse")
-  fasta_path = here("mouse_cortex", "salmon_files", "gencode.vM25.transcripts.mouse.fa.gz")
-  gtf_path = here("mouse_cortex", "salmon_files", "gencode.vM25.annotation.gtf.gz") 
-  json_file = here("mouse_cortex", "salmon_files", paste0(basename(index_dir), ".json"))
-  makeLinkedTxome(indexDir=index_dir,
-                  source="other", organism="mouse",
-                  release="other", genome="GRCm38",
-                  fasta=fasta_path, gtf=gtf_path,
-                  jsonFile=json_file) # this command will add the index to the cache automatically
-  
-} else if(pipeline == "preandmrna"){
-  index_dir = here("mouse_cortex", "salmon_files", "gencode.vM25_salmon-index-v1.0.0-preandmrna-mouse")
-  fasta_path = here("mouse_cortex", "salmon_files", "gencode.vM25.preandmrna.mouse.fa.gz")
-  gtf_path = here("mouse_cortex", "salmon_files", "gencode.vM25.annotation.gtf.gz") 
-  json_file = here("mouse_cortex", "salmon_files", paste0(basename(index_dir), ".json"))
-  makeLinkedTxome(indexDir=index_dir,
-                  source="other", organism="mouse",
-                  release="other", genome="GRCm38",
-                  fasta=fasta_path, gtf=gtf_path,
-                  jsonFile=json_file) # this command will add the index to the cache automatically
-} else if(pipeline == "introncollapse"){
-  index_dir = here("mouse_cortex", "salmon_files", "gencode.vM25_salmon-index-v1.0.0-introncollapse-mouse")
-  fasta_path = here("mouse_cortex", "salmon_files", "gencode.vM25.introncollapse.mouse.fa.gz")
-  gtf_path = here("mouse_cortex", "salmon_files", "gencode.vM25.annotation.gtf.gz") 
-  json_file = here("mouse_cortex", "salmon_files", paste0(basename(index_dir), ".json"))
-  makeLinkedTxome(indexDir=index_dir,
-                  source="other", organism="mouse",
-                  release="other", genome="GRCm38",
-                  fasta=fasta_path, gtf=gtf_path,
-                  jsonFile=json_file) # this command will add the index to the cache automatically
-} else if(pipeline == "intronseparate"){
-  index_dir = here("mouse_cortex", "salmon_files", "gencode.vM25_salmon-index-v1.0.0-intronseparate-mouse")
-  fasta_path = here("mouse_cortex", "salmon_files", "gencode.vM25.intronseparate.mouse.fa.gz")
-  gtf_path = here("mouse_cortex", "salmon_files", "gencode.vM25.annotation.gtf.gz") 
-  json_file = here("mouse_cortex", "salmon_files", paste0(basename(index_dir), ".json"))
-  makeLinkedTxome(indexDir=index_dir,
-                  source="other", organism="mouse",
-                  release="other", genome="GRCm38",
-                  fasta=fasta_path, gtf=gtf_path,
-                  jsonFile=json_file) # this command will add the index to the cache automatically
-}
+# load linkedTxome json file if not already in cache (it should be in cache when you create it in quantify-salmon.Rmd)
+# if(pipeline == "transcripts"){
+#   json_file = here("mouse_cortex", "salmon_files", "gencode.vM25_salmon-index-v1.0.0-transcripts-mouse.json")
+#   json_file = "/fastscratch/myscratch/akuo/alsf-filbin/mouse_cortex/salmon_files/gencode.vM25_salmon-index-v1.0.0-transcripts-mouse.json"
+#   loadLinkedTxome(json_file)
+# } else if(pipeline == "preandmrna"){
+#   json_file = here("mouse_cortex", "salmon_files", "gencode.vM25_salmon-index-v1.0.0-preandmrna-mouse.json")
+#   loadLinkedTxome(json_file)
+# } else if(pipeline == "introncollapse"){
+#   json_file = here("mouse_cortex", "salmon_files", "gencode.vM25_salmon-index-v1.0.0-introncollapse-mouse.json")
+#   loadLinkedTxome(json_file)
+# } else if(pipeline == "intronseparate"){
+#   json_file = here("mouse_cortex", "salmon_files", "gencode.vM25_salmon-index-v1.0.0-intronseparate-mouse.json")
+#   loadLinkedTxome(json_file)
+# }
 
 # Import with tximeta
 # Note: alevin import currently only supports a single experiment at a time
 run_names = gsub("_quant", "", basename(list.dirs(here("mouse_cortex", "salmon_quants", paste0(pipeline, "_pipeline")), recursive = F)))
 run_names = run_names[!grepl("nodecoys", run_names)]
+
 file_paths = here("mouse_cortex", "salmon_quants", paste0(pipeline, "_pipeline"), paste0(run_names, "_quant"), "alevin", "quants_mat.gz") 
 
 for(i in 1:length(run_names)){
+  print(file_paths[i])
   se = tximeta(coldata = data.frame(names = run_names[i],
                                     files = file_paths[i],
                                     stringsAsFactors = FALSE),
