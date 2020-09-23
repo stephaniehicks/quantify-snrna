045608a2278e7ee865bf2d65004c5447e747f088
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-09-23 19:21:48 -0400

    Change line size

--- mouse_cortex/code/distribution-plots-helpers.R
+++ mouse_cortex/code/distribution-plots-helpers.R
@@ -55,8 +55,7 @@ plot_prob = function(dat_sub){
   plt = plot_lines_dt %>%
     ggplot(aes(x = means, y = probs_0)) +
     geom_point(data = plot_dt, aes(x = means, y = emp_probs_0), alpha = 0.4) + # Add data points
-    geom_line(aes(color = model),
-              size = 1) + # Add lines for models
+    geom_line(aes(color = model)) + # Add lines for models
     scale_x_log10() +
     labs(x = "Log of mean expression",
          y = "Fraction of zeros droplets") +
