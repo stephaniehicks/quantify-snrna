5576fa3f6daf52de572676dd4f7143e28939ca90
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-11-16 18:12:18 -0500

    Initial commit

---
+++ mouse_cortex/code/de-analysis.sh
@@ -0,0 +1,11 @@
+#$ -cwd
+#$ -o log/
+#$ -e log/
+#$ -l mem_free=20G,h_vmem=20G
+
+# set project directory path
+d=/fastscratch/myscratch/akuo/alsf-filbin
+
+# run script
+module load conda_R/4.0
+R CMD BATCH de-analysis.R
