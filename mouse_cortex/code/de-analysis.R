2b2566b065d8bd8601371e5c2e1b2610bf1c459d
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-11-23 15:08:41 -0500

    Add normalization with edaseq

--- mouse_cortex/code/de-analysis.R
+++ mouse_cortex/code/de-analysis.R
@@ -1,7 +1,7 @@
 # de-analysis.R
 # -----------------------------------------------------------------------------
 # Author:             Albert Kuo
-# Date last modified: Nov 16, 2020
+# Date last modified: Nov 23, 2020
 #
 # Differential expression analysis
 
@@ -10,6 +10,7 @@ suppressPackageStartupMessages({
   library(tidyverse)
   library(SingleCellExperiment)
   library(DESeq2)
+  library(EDASeq)
   library(apeglm)
   library(tictoc)
 })
@@ -23,18 +24,41 @@ sce_ls[["preandmrna"]] = readRDS(here("mouse_cortex", "salmon_quants", "preandmr
 sce_ls[["introncollapse"]] = readRDS(here("mouse_cortex", "salmon_quants", "introncollapse_pipeline", paste0("sce_", run_number, ".rds")))
 sce_ls[["intronseparate"]] = readRDS(here("mouse_cortex", "salmon_quants", "intronseparate_pipeline", paste0("sce_", run_number, ".rds")))
 
-
-
 pipeline = "transcripts"
 sce_sub = sce_ls[[pipeline]][, colData(sce_ls[[pipeline]])$ding_labels %in% c("Excitatory neuron", "Inhibitory neuron")]
 
+# Get gene lengths
+genes_length_tb = rowData(sce_sub) %>%
+  as_tibble() %>%
+  select(start, end) %>%
+  mutate(length = end - start,
+         gene = rownames(rowData(sce_ls[["transcripts"]]))) %>%
+  select(gene, length)
+genes_length_tb = as.data.frame(genes_length_tb)
+rownames(genes_length_tb) = genes_length_tb$gene
+
+# Normalize with EDASeq
+seq_data = newSeqExpressionSet(counts = as.matrix(counts(sce_sub)),
+                               featureData = as.data.frame(genes_length_tb),
+                               phenoData = as.data.frame(colData(sce_sub)))
+tic("normalize with EDASeq")
+seq_data_within = withinLaneNormalization(seq_data, "length", which = "full", offset = TRUE)
+toc()
+
 # Store counts and intermediate quantities (dds is a SummarizedExperiment)
-dds = DESeqDataSetFromMatrix(countData = ceiling(counts(sce_sub)[, ]), # take integers 
-                             colData = colData(sce_sub),
+# dds = DESeqDataSetFromMatrix(countData = ceiling(counts(sce_sub)[, ]), # take integers 
+#                              colData = colData(sce_sub),
+#                              design = ~ cortex + ding_labels)
+
+dds = DESeqDataSetFromMatrix(countData = ceiling(counts(seq_data_within)[, ]), # take integers 
+                             colData = pData(seq_data_within),
                              design = ~ cortex + ding_labels)
+normFactors <- exp(-1 * offst(seq_data_within))
+normFactors <- normFactors / exp(rowMeans(log(normFactors)))
+normalizationFactors(dds) <- normFactors
 
 # Compute log2fold change and p-values
-tic()
+tic("DEseq")
 dds = DESeq(dds)
 toc()
 
