0813979163aa8ea6a66df8b733069cb485869f8c
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2022-05-20 12:51:17 -0400

    Fix binomial individual components

--- mouse_cortex/code/distribution-plots-helpers.R
+++ mouse_cortex/code/distribution-plots-helpers.R
@@ -115,12 +115,14 @@ mult_bic<-function(m){
   n<-colSums(m)
   p<-rowSums(m)/sum(n)
   # Return individual components
-  # tmp = vector(mode = "list", length = nrow(m))
-  # ncolm = ncol(m)
-  # for(i in 1:nrow(m)){
-  #   tmp[[i]] = dmultinom(m[i, ], prob = rep(p[i], ncolm), log = TRUE)
-  # }
-  # return(tmp)
+  tmp = matrix(numeric(nrow(m)*ncol(m)), nrow = nrow(m), ncol = ncol(m))
+  ncolm = ncol(m)
+  for(i in 1:nrow(m)){
+    for(j in 1:ncol(m)){
+      tmp[i,j] = dbinom(m[i, j], prob = p[i], size = n[j], log = TRUE)
+    }
+  }
+  return(rowSums(tmp))
   ll<-sum(apply(m,2,dmultinom,prob=p,log=TRUE))
   df<-nrow(m)-1
   print(paste0("Loglikelihood is ", ll, " df is ", df))
@@ -171,7 +173,7 @@ poi_bic<-function(m,X=NULL,prefit=NULL,maxit=100){
     prefit<-poi_fit(m,X,maxit=maxit)
   }
   # Return individual components
-  # return(prefit$ll)
+  return(prefit$ll)
   ll<-sum(prefit$ll) # Sum the sums of loglikelihoods across genes
   #compute BIC: -2*loglik+df*log(n_obs)
   print(paste0("Loglikelihood is ", ll, " df is ", df))
@@ -194,7 +196,7 @@ nb_bic_1 = function(m){
   # get ll
   ll = matrix(dnbinom(m, size = phi, mu = mu, log = TRUE), nrow = nrow(m)) # get likelihood for every cell x gene
   # Return individual components
-  # return(rowSums(ll))
+  return(rowSums(ll))
   ll = sum(ll) # sum likelihood
   df = nrow(m) + 1
   print(paste0("Loglikelihood is ", ll, " df is ", df))
@@ -232,7 +234,7 @@ nb_bic_2<-function(m,X=NULL,prefit=NULL){
     stopifnot(nrow(m)==nrow(prefit))
   }
   # Return individual components
-  # return(prefit$ll)
+  return(prefit$ll)
   ll<-sum(prefit$ll)
   k<-if(is.null(X)){ 2 } else { ncol(X)+1 }
   df = k*nrow(m)
